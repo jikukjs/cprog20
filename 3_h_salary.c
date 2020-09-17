9e91501cedbf3c8e84cf17326c850a2162b5e395
jikukjs <jikukjs7@gmail.com>
2020-09-18 01:29:56 +0900

    3_h_salary.c added

---
+++ 3_h_salary.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define TAX_RATE 0.2
+
+int main ()
+{
+	const int MONTHS =12;
+	int m_salary, y_salary;
+
+	printf("월급을 입력하시요:");
+	scanf("%d",&m_salary);
+
+	y_salary = MONTHS * m_salary;
+
+	printf("연봉은 %d입니다.", y_salary);
+ 	printf("세금은 %f입니다.", y_salary*TAX_RATE);
+}
