c3cc05fa72fc7b9c90221e19577e4ceb677fc21c
jikukjs <jikukjs7@gmail.com>
2020-09-18 01:30:32 +0900

    3_h_sum.c added

---
+++ 3_h_sum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main ()
+{
+	int x,y,z;
+	int sum = 0;
+
+	printf("3개의 정수를 입력하세요 (x,y,z): ");
+	scanf("%d %d %d", &x, &y, &z);
+	sum += x;
+	sum += y;
+	sum += z;
+	printf("3개정수의 합은 %d\n", sum);
+
+}
