0217a1ee8ca065982814f27e1f93a40dde4f8bf8
jikukjs <jikukjs7@gmail.com>
2020-11-01 23:01:01 +0900

    10_4.c added

---
+++ 10_4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 6
+int main(void)
+{
+	int freq[SIZE] = { 0 }; 
+	int i;
+	for(i = 0; i < 10000; i++) 
+		++freq[ rand() % 6 ]; 
+
+	for(i = 0; i < SIZE; i++)
+		printf("%3d %3d \n", i, freq[i]);
+
+}
